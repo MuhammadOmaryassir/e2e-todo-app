7943b49f590f668b671dc3be28b8c347ca7f0f97
Muhammad Omar <muhammad.omar.eg@gmail.com>
2022-10-25 16:36:00 +0200

    Update todo Controller

--- todo-api/src/todos/todos.controller.ts
+++ todo-api/src/todos/todos.controller.ts
@@ -6,8 +6,20 @@ import { TodosService } from './todos.service';
 export class TodosController {
   constructor(private todosService: TodosService) {}
   @Post()
-  createTodo(@Body() body: CreateTodoDto) {
-    return this.todosService.create(body);
+  async createTodo(@Body() body: CreateTodoDto) {
+    let todo = await this.todosService.create(body);
+    if(todo) {
+      return {
+        data:todo,
+        status:200,
+        message:"Creaeted Sucessfully"
+      }
+    }
+    return {
+      data:[],
+      status:400,
+      message:"Somthing Went worng"
+    }
   }
   @Get()
   async findTodos() {
@@ -31,12 +43,36 @@ export class TodosController {
     return this.todosService.findOne(id);
   }
   @Patch('/:id')
-  updateTodoStatus(@Param('id') id: number) {
-    return this.todosService.update(id);
+  async updateTodoStatus(@Param('id') id: number) {
+    let updatedTodo = await this.todosService.update(id)
+    if(updatedTodo) {
+      return {
+        data:updatedTodo,
+        status:200,
+        message:"Updated Sucessfully"
+      }
+    }
+    return {
+      data:[],
+      status:400,
+      message:"Somthing Went worng"
+    }
   }
 
   @Delete('/:id')
-  deleteTodo(@Param('id') id: number) {
-    return this.todosService.delete(id)
+  async deleteTodo(@Param('id') id: number) {
+    let deleedTodo = await this.todosService.delete(id)
+    if(deleedTodo) {
+      return {
+        data:deleedTodo,
+        status:200,
+        message:"Updated Sucessfully"
+      }
+    }
+    return {
+      data:[],
+      status:400,
+      message:"Somthing Went worng"
+    }
   }
 }
